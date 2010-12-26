69e398b10026600459223706f59b7a916fd1f422
Achim Lörke <achim@loerke.net>
2010-12-26 16:57:20 +0100

    empty RCP application

---
+++ net.loerke.itemlist.ui/src/net/loerke/itemlist/ui/intro/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,24 @@
+package net.loerke.itemlist.ui.intro;
+
+import org.eclipse.ui.application.IWorkbenchConfigurer;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "net.loerke.itemlist.ui.perspective";
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+    
+    public void initialize(IWorkbenchConfigurer configurer) {
+        super.initialize(configurer);
+        configurer.setSaveAndRestore(true);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
